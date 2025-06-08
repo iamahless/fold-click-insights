d4571558819d2436cd59772405cc303daa9209b0
Alexander <alexandergaruba96@gmail.com>
2025-06-08 12:10:15 +0100

    init commit

---
+++ fold-click-insights.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     FoldClick Insights
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Nothing
+ * Author:          Alexander Garuba
+ * Author URI:      https://github.com/iamahless
+ * Text Domain:     fold-click-insights
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fold_Click_Insights
+ */
+
+// Your code starts here.
